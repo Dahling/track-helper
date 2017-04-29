1ca0970fc8b8f30e66e30e9d6a995edb9531f819
Jenna Cooper <jchristinec@gmail.com>
2017-04-28 18:46:08 -0700

    Add all of the js that I've added, as well as an additional question to the index

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,50 @@
 $(document).ready(function() {
 
   $("form#tracksuggester").submit(function(event) {
+    var development = $("select#development").val();
     var platform = $("select#platform").val();
     var career = $("select#career").val();
     var personality = $("select#personality").val();
 
     if (platform === "windows") {
-      if (career === "developer" ) {
-        $("#track").empty().append("Track1");
-      } else if (career === "startup") {
-        $("#track").empty().append("Track2");
+      if (development === "front-end" ) {
+        $("#track").empty().append("CSS/Design");
+      } else if (development === "back-end") {
+        $("#track").empty().append("Ruby");
+      } else if (career === "large") {
+        $("#track").empty().append("C#");
+      } else if (career === "small") {
+        $("#track").empty().append("Ruby");
+      } else if (personality === "visual") {
+        $("#track").empty().append("CSS/Design");
       } else {
-        $("#track").empty().append("Track3");
+        $("#track").empty().append("C#, Java, PHP, or Ruby");
       }
     } else if (platform === "mac") {
-      if (career === "developer" ) {
-        $("#track").empty().append("Track1");
-      } else if (career === "startup") {
-        $("#track").empty().append("Track2");
+      if (development === "front-end" ) {
+        $("#track").empty().append("CSS/Design");
+      } else if (development === "back-end") {
+        $("#track").empty().append("Ruby");
+      } else if (career === "large") {
+        $("#track").empty().append("PHP");
+      } else if (career === "small") {
+        $("#track").empty().append("Ruby");
       } else {
         $("#track").empty().append("Track3");
       }
     } else if (platform === "android"){
-      if (career === "developer" ) {
-        $("#track").empty().append("Track1");
-      } else if (career === "startup") {
-        $("#track").empty().append("Track2");
+      if (development === "front-end" ) {
+        $("#track").empty().append("CSS/Design");
+      } else if (development === "back-end") {
+        $("#track").empty().append("Java");
+      } else if (career === "large") {
+        $("#track").empty().append("Java");
+      } else if (career === "small") {
+        $("track").empty().append("Java")
+      } else if (personality === "visual") {
+        $("#track").empty().append("CSS/Design");
       } else {
-        $("#track").empty().append("Track3");
+        $("#track").empty().append("C#, Java, PHP, or Ruby");
       }
     } else {
       alert('Please enter your age.');
